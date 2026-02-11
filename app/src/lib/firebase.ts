376d5821c5ac5fe2e9da6fd605e904182f06ca83
ArnavGupta23 <arnavkgu@gmail.com>
2026-02-11 10:52:08 -0500

    base configs

---
+++ app/src/lib/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCraHRWoXuc5FotA6IGvWk-D-iPrwGRMWk",
+  authDomain: "kfk-gift-registry.firebaseapp.com",
+  projectId: "kfk-gift-registry",
+  storageBucket: "kfk-gift-registry.firebasestorage.app",
+  messagingSenderId: "1061530360755",
+  appId: "1:1061530360755:web:8d972140ad02140e1149c6",
+  measurementId: "G-5W4T36409S"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
