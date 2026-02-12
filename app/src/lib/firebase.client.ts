36e4556c8b2208d4e8238d585faddfa428cf5da6
Ramy Kaddouri <myramkadd@gmail.com>
2026-02-11 23:22:07 -0500

    only load auth/db instance once + split up server fns

--- app/src/lib/firebase.client.ts
+++ app/src/lib/firebase.client.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { Auth, getAuth } from "firebase/auth";
 import { createClientOnlyFn } from "@tanstack/react-start";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,20 +7,22 @@ import { createClientOnlyFn } from "@tanstack/react-start";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "kfk-gift-registry.firebaseapp.com",
+  projectId: "kfk-gift-registry",
+  storageBucket: "kfk-gift-registry.firebasestorage.app",
+  messagingSenderId: "1061530360755",
+  appId: "1:1061530360755:web:8d972140ad02140e1149c6",
+  measurementId: "G-5W4T36409S"
+};
+
+const app = initializeApp(firebaseConfig);
+let auth: Auth | null = null
 
 export const getClientAuth = createClientOnlyFn(() => {
-  const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "kfk-gift-registry.firebaseapp.com",
-    projectId: "kfk-gift-registry",
-    storageBucket: "kfk-gift-registry.firebasestorage.app",
-    messagingSenderId: "1061530360755",
-    appId: "1:1061530360755:web:8d972140ad02140e1149c6",
-    measurementId: "G-5W4T36409S"
-  };
-
-  const app = initializeApp(firebaseConfig);
-
-  return getAuth(app);
-  })
+  if (auth) return auth;
+  auth = getAuth(app);
+  return auth;
+})
 
--- app/src/lib/firebase.server.ts
+++ app/src/lib/firebase.server.ts
@@ -1,13 +1,22 @@
 import { createServerOnlyFn } from "@tanstack/react-start";
 import * as admin from 'firebase-admin'
 
-export const getAdmin = createServerOnlyFn(() => {
-  if (!admin.apps.length || admin.apps.length === 0) {
-    admin.initializeApp()
-  }
+if (!admin.apps.length || admin.apps.length === 0) {
+  admin.initializeApp()
+}
 
-  return {
-    db: admin.firestore(),
-    auth: admin.auth(),
-  }
+let auth: admin.auth.Auth | null = null;
+let db: admin.firestore.Firestore | null;
+
+export const getServerAuth = createServerOnlyFn(() => {
+  if (auth) return auth;
+  auth = admin.auth();
+  return auth;
+})
+
+export const getServerDB = createServerOnlyFn(() => {
+  if (db) return db;
+
+  db = admin.firestore();
+  return db;
 })
