be4ae46b53b73ce2076c31227d1c9515b998894c
Ramy Kaddouri <myramkadd@gmail.com>
2026-02-14 14:36:49 -0500

    admin app init fix

--- app/src/lib/firebase.server.ts
+++ app/src/lib/firebase.server.ts
@@ -4,21 +4,17 @@ import admin from "firebase-admin";
 let auth: admin.auth.Auth | null = null;
 let db: admin.firestore.Firestore | null = null;
 
-const initApp = () => {
-  if (!admin.apps?.length) {
-    admin.initializeApp();
-  }
-};
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 
 export const getServerAuth = createServerOnlyFn(() => {
-  initApp();
   if (auth) return auth;
   auth = admin.auth();
   return auth;
 });
 
 export const getServerDB = createServerOnlyFn(() => {
-  initApp();
   if (db) return db;
 
   db = admin.firestore();
