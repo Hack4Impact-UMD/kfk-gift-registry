bb5782e7573d95b541bf52478df6d3ba189a9f0e
Maggie McAndrew <you@example.com>
2026-02-22 02:01:40 -0500

    Adjusted test data for better PR visual

--- app/src/routes/data.tsx
+++ app/src/routes/data.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { ApprovedProfilesTable } from '@/components/tables/ApprovedProfilesTable/ApprovedProfilesTable'
 
-//Testing data on profiles table
+// Testing data on profiles table
 const testData = [
   {
     id: '1',
@@ -12,19 +12,41 @@ const testData = [
     diagnosis: 'test diagnosis',
     type: 'warrior' as const,
     giftsFulfilled: 2,
-    giftsTotal: 5,
+    giftsTotal: 3,
   },
   {
     id: '2',
-    childName: 'Rainbow Waterfalls',
-    parentGuardian: 'Kevin Waterfalls',
-    email: 'kev@example.com',
+    childName: 'Bob Marley',
+    parentGuardian: 'Bobs Dad',
+    email: 'bobdad@example.com',
     age: 11,
+    diagnosis: 'test diagnosis',
+    type: 'warrior' as const,
+    giftsFulfilled: 3,
+    giftsTotal: 3,
+  },
+  {
+    id: '3',
+    childName: 'Sis Marley',
+    parentGuardian: 'Bobs Dad',
+    email: 'bobdad@example.com',
+    age: 6,
     diagnosis: 'test diagnosis',
     type: 'supersib' as const,
     giftsFulfilled: 0,
     giftsTotal: 3,
-  }
+  },
+  {
+    id: '4',
+    childName: 'Sis2 Marley',
+    parentGuardian: 'Bobs Dad',
+    email: 'bobdad@example.com',
+    age: 5,
+    diagnosis: 'test diagnosis',
+    type: 'supersib' as const,
+    giftsFulfilled: 1,
+    giftsTotal: 3,
+  },
 ]
 
 export const Route = createFileRoute('/data')({
