fa77fef3609a207b5de6348b4346152d1984ef95
Ramy Kaddouri <myramkadd@gmail.com>
2026-02-22 21:32:15 -0500

    add default button styles

--- app/src/components/tables/ApprovedProfilesTable/ApprovedProfilesTable.tsx
+++ app/src/components/tables/ApprovedProfilesTable/ApprovedProfilesTable.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Search, Undo2, ExternalLink } from 'lucide-react';
+import { ExternalLink, Search, Undo2 } from 'lucide-react';
 import { DataTable } from "../DataTable";
 import { columns } from "./columns";
 import { Button } from "@/components/ui/button";
@@ -18,30 +18,30 @@ export type ApprovedProfileTableRow = {
 };
 
 interface ApprovedProfilesTableProps {
-  data: ApprovedProfileTableRow[];
+  data: Array<ApprovedProfileTableRow>;
 }
 
 export function ApprovedProfilesTable({ data }: ApprovedProfilesTableProps) {
   const [globalSearch, setGlobalSearch] = useState('')
-  
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center mt-6">
         <div className="relative flex items-center mr-auto ml-6 w-48">
-          <Search className="absolute ml-2 h-4 w-4 text-gray-500"/>
+          <Search className="absolute ml-2 h-4 w-4 text-gray-500" />
           <Input
-            placeholder="Search" 
+            placeholder="Search"
             value={globalSearch}
-            onChange={(e) => setGlobalSearch(e.target.value)} 
+            onChange={(e) => setGlobalSearch(e.target.value)}
             className="pl-7 border-gray-400 text-gray-500"
           />
         </div>
         <div className="flex gap-6 ml-auto mr-6">
-          <Button variant="outline" className="border-[#0839B1] text-[#0839B1] font-medium">
+          <Button variant="outline">
             <Undo2 className="h-4 w-4" />
             Withdraw Approval
           </Button>
-          <Button className="bg-[#0839B1] font-medium">
+          <Button>
             <ExternalLink className="h-4 w-4" />
             Export
           </Button>
