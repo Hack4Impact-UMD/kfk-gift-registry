988bbe4a84747922b0ba55149d2646f9659dd5ab
Nadia Meyerovich <nmeyerov@terpmail.umd.edu>
2026-02-24 15:43:52 -0500

    clean up

--- app/src/components/StaffSidebar/StaffSidebar.tsx
+++ app/src/components/StaffSidebar/StaffSidebar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useRouteContext, useMatchRoute } from "@tanstack/react-router";
+import { Link, useRouteContext } from "@tanstack/react-router";
 import { logout } from "@/services/authService.client";
 import KFKLogo from "@/assets/kfk-logo.png";
 
@@ -60,13 +60,13 @@ function SidebarMenuButtonWithTooltip({
 }
 
 const SidebarIcon = ({
-  isPressed,
+  active = false,
   activeColor,
   size,
   children,
   ...props
 }: {
-  isPressed: boolean;
+  active?: boolean;
   size?: string;
   children: React.ReactNode;
   activeColor: string;
@@ -79,8 +79,7 @@ const SidebarIcon = ({
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        // className={`transition-colors duration-500 text-gray-500 group-hover/button:text-black group-data-[status=active]/button:${activeColor} ${size || "size-6"}`}
-        className={`transition-colors duration-500 ${isPressed ? activeColor : 'text-gray-500'} group-data-[status=active]/button:${activeColor} group-hover/button:text-black ${size || "size-6"}`}
+        className={`transition-colors duration-500 ${active ? activeColor : 'text-gray-500'} group-data-[status=active]/button:${activeColor} group-hover/button:text-black ${size || "size-6"}`}
       >
         {children}
       </svg>
@@ -89,23 +88,19 @@ const SidebarIcon = ({
 };
 
 interface Props {
-  children: (isPressed: boolean) => React.ReactNode;
+  children: React.ReactNode;
 }
 const SidebarMenuButtonWithHovering = ({ children }: Props) => {
-  const [isPressed, setIsPressed] = useState<boolean>(false);
 
   return (
     <SidebarMenuButton
       asChild
       size={"lg"}
-      onMouseDown={() => setIsPressed(true)}
-      onMouseLeave={() => setIsPressed(false)}
-      isActive={isPressed}
       className={
         "group/button transition min-h-12 min-w-12 flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
       }
     >
-      {children(isPressed)}
+      {children}
     </SidebarMenuButton>
   );
 };
@@ -122,7 +117,7 @@ export function StaffSidebar() {
     <Sidebar collapsible="icon" className="duration-200">
       <SidebarTrigger
         openIcon={
-          <SidebarIcon isPressed={false} activeColor="text-black">
+          <SidebarIcon active={false} activeColor="text-black">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -131,7 +126,7 @@ export function StaffSidebar() {
           </SidebarIcon>
         }
         closeIcon={
-          <SidebarIcon isPressed={false} activeColor="text-black">
+          <SidebarIcon active={false} activeColor="text-black">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -170,7 +165,7 @@ export function StaffSidebar() {
                   <SidebarMenuButtonWithTooltip label={year}>
                     <SelectTrigger className="w-full group/button flex items-center justify-start gap-2 group-data-[collapsible=icon]:min-w-12 min-h-12 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:p-0 [&>svg]:group-data-[collapsible=icon]:hidden">
                       <SidebarIcon
-                        isPressed={isDropdownPressed}
+                        active={isDropdownPressed}
                         activeColor="text-[var(--color-kfk-blue)]"
                       >
                         <path
@@ -196,128 +191,116 @@ export function StaffSidebar() {
             <SidebarMenuItem className="flex justify-center">
               <SidebarMenuButtonWithTooltip label="Home">
                 <SidebarMenuButtonWithHovering>
-                  {(isPressed) => (
-                    <Link
-                      to="/staff/home"
-                      className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
-                      activeProps={{
-                        className: "group/button flex items-center gap-2 text-[var(--color-kfk-red)]",
-                      }}
+                  <Link
+                    to="/staff/home"
+                    className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
+                    activeProps={{
+                      className: "group/button flex items-center gap-2 text-[var(--color-kfk-red)]",
+                    }}
+                  >
+                    <SidebarIcon
+                      activeColor="text-[var(--color-kfk-red)]"
                     >
-                      <SidebarIcon
-                        isPressed={isPressed}
-                        activeColor="text-[var(--color-kfk-red)]"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                        />
-                      </SidebarIcon>
-                      <span
-                        className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 group-data-[status=active]/button:text-[var(--color-kfk-red)]`}
-                      >
-                        Home
-                      </span>
-                    </Link>
-                  )}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                      />
+                    </SidebarIcon>
+                    <span
+                      className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 group-data-[status=active]/button:text-[var(--color-kfk-red)]`}
+                    >
+                      Home
+                    </span>
+                  </Link>
                 </SidebarMenuButtonWithHovering>
               </SidebarMenuButtonWithTooltip>
             </SidebarMenuItem>
             <SidebarMenuItem className="flex justify-center">
               <SidebarMenuButtonWithTooltip label="Profile Approval">
                 <SidebarMenuButtonWithHovering>
-                  {(isPressed) => (
-                    <Link
-                      to="/staff/home"
-                      className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
-                      activeProps={{
-                        className: "group/button flex items-center gap-2 text-[var(--color-kfk-yellow)]",
-                      }}
+                <Link
+                    to="/staff/home"
+                    className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
+                    activeProps={{
+                      className: "group/button flex items-center gap-2 text-[var(--color-kfk-yellow)]",
+                    }}
+                  >
+                    {" "}
+                    {/* Placeholder Link */}
+                    <SidebarIcon
+                      activeColor="text-[var(--color-kfk-yellow)]"
                     >
-                      {" "}
-                      {/* Placeholder Link */}
-                      <SidebarIcon
-                        isPressed={isPressed}
-                        activeColor="text-[var(--color-kfk-yellow)]"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
-                        />
-                      </SidebarIcon>
-                      <span
-                        className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 ease-in-out group-data-[status=active]/button:text-[var(--color-kfk-yellow)]`}
-                      >
-                        Profile Approval
-                      </span>
-                    </Link>
-                  )}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
+                      />
+                    </SidebarIcon>
+                    <span
+                      className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 ease-in-out group-data-[status=active]/button:text-[var(--color-kfk-yellow)]`}
+                    >
+                      Profile Approval
+                    </span>
+                  </Link>
                 </SidebarMenuButtonWithHovering>
               </SidebarMenuButtonWithTooltip>
             </SidebarMenuItem>
             <SidebarMenuItem className="flex justify-center">
               <SidebarMenuButtonWithTooltip label="Approved Profiles">
                 <SidebarMenuButtonWithHovering>
-                  {(isPressed) => (
-                    <Link
-                      to="/staff/approved"
-                      className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
-                      activeProps={{
-                        className: "group/button flex items-center gap-2 text-[var(--color-kfk-blue)]",
-                      }}
+                  <Link
+                    to="/staff/approved"
+                    className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
+                    activeProps={{
+                      className: "group/button flex items-center gap-2 text-[var(--color-kfk-blue)]",
+                    }}
+                  >
+                    <SidebarIcon
+                      activeColor="text-[var(--color-kfk-blue)]"
                     >
-                      <SidebarIcon
-                        isPressed={isPressed}
-                        activeColor="text-[var(--color-kfk-blue)]"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75"
-                        />
-                      </SidebarIcon>
-                      <span
-                        className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 ease-in-out group-data-[status=active]/button:text-[var(--color-kfk-blue)]`}
-                      >
-                        Approved Profiles
-                      </span>
-                    </Link>
-                  )}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75"
+                      />
+                    </SidebarIcon>
+                    <span
+                      className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 ease-in-out group-data-[status=active]/button:text-[var(--color-kfk-blue)]`}
+                    >
+                      Approved Profiles
+                    </span>
+                  </Link>
                 </SidebarMenuButtonWithHovering>
               </SidebarMenuButtonWithTooltip>
             </SidebarMenuItem>
             <SidebarMenuItem className="flex justify-center">
               <SidebarMenuButtonWithTooltip label="User Management">
                 <SidebarMenuButtonWithHovering>
-                  {(isPressed) => (
-                    <Link
-                      to="/staff/home"
-                      className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
-                      activeProps={{
-                        className: "group/button flex items-center gap-2 text-[var(--color-kfk-green)]",
-                      }}
+                  <Link
+                    to="/staff/home"
+                    className="group/button flex items-center gap-2 group-data-[collapsible=icon]:justify-center"
+                    activeProps={{
+                      className: "group/button flex items-center gap-2 text-[var(--color-kfk-green)]",
+                    }}
+                  >
+                    {" "}
+                    {/* Placeholder Link */}
+                    <SidebarIcon
+                      activeColor="text-[var(--color-kfk-green)]"
                     >
-                      {" "}
-                      {/* Placeholder Link */}
-                      <SidebarIcon
-                        isPressed={isPressed}
-                        activeColor="text-[var(--color-kfk-green)]"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-                        />
-                      </SidebarIcon>
-                      <span
-                        className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 ease-in-out group-data-[status=active]/button:text-[var(--color-kfk-green)]`}
-                      >
-                        User Management
-                      </span>
-                    </Link>
-                  )}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                      />
+                    </SidebarIcon>
+                    <span
+                      className={`group-data-[collapsible=icon]:hidden transition-colors duration-500 ease-in-out group-data-[status=active]/button:text-[var(--color-kfk-green)]`}
+                    >
+                      User Management
+                    </span>
+                  </Link>
                 </SidebarMenuButtonWithHovering>
               </SidebarMenuButtonWithTooltip>
             </SidebarMenuItem>
@@ -329,7 +312,7 @@ export function StaffSidebar() {
           <SidebarMenuButtonWithTooltip label={user?.displayName || "User"}>
             <PopoverTrigger asChild>
               <button className="flex items-center gap-3 w-full text-left">
-                <SidebarIcon isPressed={true} size="size-10" activeColor="text-black">
+                <SidebarIcon active={true} size="size-10" activeColor="text-black">
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
