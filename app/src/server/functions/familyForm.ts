dba68ed6f5500a6cc6b4c9cc73f2558e0e853b7a
Ori Marx <orimarx@gmail.com>
2026-03-20 16:38:33 -0400

    general schema prep (based on types in common)

--- app/src/server/functions/familyForm.ts
+++ app/src/server/functions/familyForm.ts
@@ -1,1 +1,65 @@
-function submitFamilyForm() {}
+import { createServerFn } from "@tanstack/react-start";
+import z from "zod";
+
+const addressSchema = z.object({
+  street: z.string(),
+  addressLine2: z.string().optional(),
+  city: z.string(),
+  state: z.string(),
+  zipCode: z.string(),
+});
+
+const generalInfoSchema = z.object({
+  contactName: z.string(),
+  email: z.string().email(),
+  phone: z.string(),
+  address: addressSchema,
+  privateNotes: z.string().optional(),
+});
+
+const childInfoSchema = z.object({
+  name: z.string(),
+  age: z.string(),
+  diagnosis: z.string().optional(),
+  hospitalTreatedAt: z.string().optional(),
+  socialWorkerName: z.string().optional(),
+  photoUrl: z.string().optional(),
+  status: z.string(),
+  treatmentLength: z.string().optional(),
+  blurb: z.string().optional(),
+  isSibling: z.boolean().optional(),
+});
+
+const childrenFormSchema = z.object({
+  numChildren: z.coerce.number(),
+  children: z.array(childInfoSchema),
+  additionalNotes: z.string().optional(),
+  consentPhotosPublic: z.boolean(),
+});
+
+const giftSelectionSchema = z.object({
+  giftUrl: z.string().url().optional(),
+  giftName: z.string().optional(),
+});
+
+const childGiftSelectionSchema = z.object({
+  childName: z.string(),
+  gifts: z.array(giftSelectionSchema),
+  backupGifts: z.array(giftSelectionSchema).optional(),
+  verified: z.boolean(),
+});
+
+const giftsFormSchema = z.object({
+  giftSelections: z.array(childGiftSelectionSchema),
+});
+
+const familyFormStateSchema = z.object({
+  generalInfo: generalInfoSchema.optional(),
+  children: childrenFormSchema.optional(),
+  gifts: giftsFormSchema.optional(),
+  consentScreen: z.boolean().optional(),
+});
+
+export type FamilyFormInput = z.infer<typeof familyFormStateSchema>; // just extracts the ts type based on zod schema
+
+export default createServerFn({ method: "POST" });
