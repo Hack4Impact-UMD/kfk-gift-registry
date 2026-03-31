3db37af2246d7b4289c5619296577373bf0b3095
Ori Marx <orimarx@gmail.com>
2026-03-31 18:38:43 -0400

    Removed unnecessary fields from mock data Fields deleted include status, trackingNumber, dateDelivered

--- app/src/components/storefront/cartMockData.ts
+++ app/src/components/storefront/cartMockData.ts
@@ -4,9 +4,6 @@ export type CartGift = {
   childName: string;
   giftName: string;
   price: number;
-  status: "unordered" | "claimed" | "in_transit" | "delivered" | "received";
-  trackingNumber?: number;
-  dateDelivered?: string;
 };
 
 export type CartFamily = {
@@ -26,9 +23,6 @@ export const mockCartData: Array<CartFamily> = [
         childName: "John R.",
         giftName: "Taco Cat Goat Cheese Pizza Card Game",
         price: 9.95,
-        status: "delivered",
-        trackingNumber: 123456789,
-        dateDelivered: "2026-10-22",
       },
       {
         id: "gift-2",
@@ -36,8 +30,6 @@ export const mockCartData: Array<CartFamily> = [
         childName: "John R.",
         giftName: "HUES and CUES - Color Board Game",
         price: 10.56,
-        status: "in_transit",
-        trackingNumber: 987654321,
       },
       {
         id: "gift-4",
@@ -45,9 +37,6 @@ export const mockCartData: Array<CartFamily> = [
         childName: "Maya R.",
         giftName: "Unicorn Academy Doll",
         price: 24.99,
-        status: "received",
-        trackingNumber: 564738291,
-        dateDelivered: "2026-10-20",
       },
       {
         id: "gift-5",
@@ -55,9 +44,6 @@ export const mockCartData: Array<CartFamily> = [
         childName: "Lily M.",
         giftName: "Mermaid Doll with Pink Hair",
         price: 19.99,
-        status: "delivered",
-        trackingNumber: 564738291,
-        dateDelivered: "2026-10-20",
       },
     ],
   },
@@ -71,7 +57,6 @@ export const mockCartData: Array<CartFamily> = [
         childName: "Emma S.",
         giftName: "Art Supply Set",
         price: 29.99,
-        status: "unordered",
       },
       {
         id: "gift-10",
@@ -79,7 +64,6 @@ export const mockCartData: Array<CartFamily> = [
         childName: "Oliver S.",
         giftName: "LEGO Space Set",
         price: 49.99,
-        status: "claimed",
       },
     ],
   },
