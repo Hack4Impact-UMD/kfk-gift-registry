ccc1c17a02011e326260bc0172494bf5313c2b42
Ori Marx <orimarx@gmail.com>
2026-04-04 17:34:14 -0400

    created and debugged getApprovedProfileTableRows

--- app/src/server/functions/child.ts
+++ app/src/server/functions/child.ts
@@ -1,6 +1,7 @@
 import { getServerDB } from "@/lib/firebase.server";
 import z from "zod";
 import { createServerFn } from "@tanstack/react-start";
+import type { ApprovedProfileTableRow } from "@/components/tables/ApprovedProfilesTable/types";
 
 const childParamSchema = z.object({ // just so it's clean for the input validator
     driveId: z.string()
@@ -29,13 +30,40 @@ export const getAllApprovedFamilyProfilesForDrive = createServerFn({
         if (familyProfiles.empty){
             throw new Error("No family profiles exist for this specific drive ID.");
         }
-        return familyProfiles.docs.map(doc => doc.data())
+        return familyProfiles.docs.map(doc => doc.data());
     })
 
-export const getApprovedProfileTableRows= createServerFn({
+export const getApprovedProfileTableRows = createServerFn({
     method: "GET"
 })
     .inputValidator(childParamSchema)
     .handler(async ({data}) => {
-        
+        const db = getServerDB();
+        const rows = [];
+        const families = await getAllApprovedFamilyProfilesForDrive({data});
+
+        for (const family of families){
+            const eachFamChildren = await db.children.where("familyId", "==", family.id).get();
+            const childrenData = eachFamChildren.docs.map(doc => doc.data());
+            for (const child of childrenData){
+                const eachChildGifts = await db.gifts.where("childId", "==", child.id).get();
+                const gifts = eachChildGifts.docs.map(doc => doc.data());
+                const row: ApprovedProfileTableRow = {
+                    id: child.id,
+                    childName: child.name,
+                    profilePictureUrl: child.photoUrl,
+                    parentGuardian: family.contactName,
+                    email: family.email,
+                    age: child.age,
+                    diagnosis: child.diagnosis,
+                    type: child.category === "warrior" ? "warrior" : "supersib",
+                    giftsFulfilled: gifts.filter(g => 
+                        ["CLAIMED", "PURCHASED", "DELIVERED", "RECEIVED"].includes(g.status)
+                    ).length,
+                    giftsTotal: gifts.length
+                };
+                rows.push(row);
+            }
+        }
+        return rows;
     })
