94fcd6d5285856206dd528bd7bc3780e8c628f66
Ori Marx <orimarx@gmail.com>
2026-04-05 23:49:59 -0400

    simple update to index.ts

--- app/src/queries/index.ts
+++ app/src/queries/index.ts
@@ -1,5 +1,6 @@
 import { mergeQueryKeys } from "@lukemorales/query-key-factory";
 import { userProfileQueries } from "./userProfileQueries";
 import { familyLinkQueries } from "./familyLink";
+import { childQueries } from "./child";
 
-export const queries = mergeQueryKeys(userProfileQueries, familyLinkQueries);
+export const queries = mergeQueryKeys(userProfileQueries, familyLinkQueries, childQueries);
