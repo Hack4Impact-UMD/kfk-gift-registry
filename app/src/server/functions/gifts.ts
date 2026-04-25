e5d0fa121e0e0f20dfa43f3ab0513b3eb2b39eda
Ori Marx <orimarx@gmail.com>
2026-04-25 13:42:16 -0400

    zod schema -> input validator (giftDrive id)

--- app/src/server/functions/gifts.ts
+++ app/src/server/functions/gifts.ts
@@ -2,8 +2,18 @@
 import { createServerFn } from "@tanstack/react-start";
 import { requireRolesMiddleware } from "@/server/middleware/authMiddleware";
 import { UserRole } from "common";
+import z from "zod";
+import { getServerDB } from "@/lib/firebase.server";
+
+const driveIdSchema = z.object({ // param for both functions
+    driveId: z.string()
+});
 
 export const getPublishedGifts = createServerFn({ method: "GET" })
     .middleware([requireRolesMiddleware([UserRole.DIRECTOR || UserRole.ADMIN])]) // no specific "staff" role so I'm reffering it to either be director or admin
+    .inputValidator(driveIdSchema)
+    .handler(async ({ data }) => {
+        const db  = getServerDB();
+    })
 
 export const getPublishedGiftsTableRows = createServerFn({ method: "GET" });
