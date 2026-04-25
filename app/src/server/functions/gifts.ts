ed3cd1b74440d52f7dacabb2d97a81929ce72358
Ori Marx <orimarx@gmail.com>
2026-04-25 13:37:34 -0400

    implemented required role middleware (for staff)

--- app/src/server/functions/gifts.ts
+++ app/src/server/functions/gifts.ts
@@ -1,5 +1,9 @@
 /* Backend server functions relating to published gifts (per specific giftDrive) */
 import { createServerFn } from "@tanstack/react-start";
+import { requireRolesMiddleware } from "@/server/middleware/authMiddleware";
+import { UserRole } from "common";
 
-export const getPublishedGifts = createServerFn({ method: "GET" });
+export const getPublishedGifts = createServerFn({ method: "GET" })
+    .middleware([requireRolesMiddleware([UserRole.DIRECTOR || UserRole.ADMIN])]) // no specific "staff" role so I'm reffering it to either be director or admin
+
 export const getPublishedGiftsTableRows = createServerFn({ method: "GET" });
