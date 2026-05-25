780d41f7e55b91204d66f5f3cb5bfcdee86578ea
Ori Marx <orimarx@gmail.com>
2026-05-25 17:38:19 -0500

    overall notification schema

--- common/src/types/notification.ts
+++ common/src/types/notification.ts
@@ -7,3 +7,16 @@ export const NotificationTypeSchema = z.enum([
 ]);
 
 export type NotificationType = z.infer<typeof NotificationTypeSchema>;
+
+export const NotificationSchema = z.object({
+  id: z.string(),
+  familyId: z.string(),
+  childId: z.string(),
+  type: NotificationTypeSchema,
+  message: z.string(),
+  giftId: z.string(),
+  createdAt: z.string().datetime(),
+  read: z.boolean(),
+});
+
+export type Notification = z.infer<typeof NotificationSchema>;
