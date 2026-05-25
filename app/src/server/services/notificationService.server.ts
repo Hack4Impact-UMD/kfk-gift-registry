1bcb7ea7befd36b3f33e825caeaefd355de30a09
Ori Marx <orimarx@gmail.com>
2026-05-25 17:45:35 -0500

    created notification service (uuid7 time-sorted id)

---
+++ app/src/server/services/notificationService.server.ts
@@ -0,0 +1,35 @@
+import { v7 as uuidv7 } from "uuid";
+import type { Notification, NotificationType } from "common";
+import { getServerDB } from "@/lib/firebase.server";
+
+export async function publishNotification(
+  tx: FirebaseFirestore.Transaction,
+  notification: Omit<Notification, "id">,
+): Promise<string> {
+  const db = getServerDB();
+  const notificationId = uuidv7();
+  
+  tx.set(db.notifications.doc(notificationId), {
+    id: notificationId,
+    ...notification,
+  });
+  
+  return notificationId;
+}
+
+export function createNotificationMessage(
+  type: NotificationType,
+  childName: string,
+  donorName?: string,
+): string {
+  switch (type) {
+    case "GIFT_CLAIMED":
+      return `${donorName || "A donor"} has claimed a gift for ${childName}!`;
+    case "GIFT_DELIVERED":
+      return `${childName}'s gift has been delivered!`;
+    case "GIFT_UPDATED":
+      return `${childName}'s gift was updated.`;
+    default:
+      return "You have a new notification.";
+  }
+}
