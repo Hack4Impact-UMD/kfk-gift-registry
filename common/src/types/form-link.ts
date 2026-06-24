147ea57c5fe1b6998f5e561403af2dfb950d71d0
Ramy Kaddouri <myramkadd@gmail.com>
2026-06-23 20:25:25 -0700

    remove refine on form link due to omit issue

--- common/src/types/form-link.ts
+++ common/src/types/form-link.ts
@@ -1,16 +1,11 @@
 import z from "zod";
 
-export const FormLinkSchema = z
-  .object({
-    id: z.string(),
-    name: z.string().optional(),
-    driveId: z.string(),
-    active: z.boolean(),
-    showOnStorefront: z.boolean(),
-  })
-  .refine((value) => !value.showOnStorefront || value.active, {
-    path: ["showOnStorefront"],
-    message: "Storefront links must be active.",
-  });
+export const FormLinkSchema = z.object({
+  id: z.string(),
+  name: z.string().optional(),
+  driveId: z.string(),
+  active: z.boolean(),
+  showOnStorefront: z.boolean(),
+});
 
 export type FormLink = z.infer<typeof FormLinkSchema>;
