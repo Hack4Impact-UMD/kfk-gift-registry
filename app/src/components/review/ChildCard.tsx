444ed4a3966d8d4e9c6f57eca6778285c4aae94f
Ramy Kaddouri <myramkadd@gmail.com>
2026-07-27 22:06:57 -0700

    fmt

--- app/src/components/review/ChildCard.tsx
+++ app/src/components/review/ChildCard.tsx
@@ -340,10 +340,11 @@ export function ChildCard({ child }: ChildCardProps) {
                 disabled={!editing}
               >
                 <Avatar
-                  className={`size-15 ${editing
-                    ? "ring-2 ring-kfk-blue/30 shadow-md hover:ring-kfk-blue"
-                    : ""
-                    }`}
+                  className={`size-15 ${
+                    editing
+                      ? "ring-2 ring-kfk-blue/30 shadow-md hover:ring-kfk-blue"
+                      : ""
+                  }`}
                 >
                   <AvatarImage
                     src={child.photoUrl || ProfileHeader}
@@ -380,10 +381,11 @@ export function ChildCard({ child }: ChildCardProps) {
                   {child.name}
                 </h2>
                 <span
-                  className={`my-auto rounded-full border border-gray-200 px-5 py-1 ${child.category == "warrior"
-                    ? "bg-[#FFF8C2] text-kfk-brown"
-                    : "bg-kfk-light-blue text-kfk-blue"
-                    }`}
+                  className={`my-auto rounded-full border border-gray-200 px-5 py-1 ${
+                    child.category == "warrior"
+                      ? "bg-[#FFF8C2] text-kfk-brown"
+                      : "bg-kfk-light-blue text-kfk-blue"
+                  }`}
                 >
                   {child.category == "warrior" ? "Warrior" : "SuperSib"}
                 </span>
@@ -540,8 +542,7 @@ export function ChildCard({ child }: ChildCardProps) {
                       draft.publicBlurb = e.target.value;
                     })
                   }
-                >
-                </EditableField>
+                ></EditableField>
               </>
             )}
           </div>
